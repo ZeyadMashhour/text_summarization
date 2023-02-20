f5a5b0c47c2c55343784139d5a85172acb0dad39
BaSsel Ahmed <88636331+Bensa0@users.noreply.github.com>
2023-02-20 22:35:32 +0200

    perfect_preprocessing
    made lemmetization
    and stopwords
    
    Co-Authored-By: ZeyadMashhour <91096543+ZeyadMashhour@users.noreply.github.com>

--- preprocessing_algorithms.py
+++ preprocessing_algorithms.py
@@ -21,43 +21,63 @@ def preprocessing_text_with_spacy(article, lemmatization = True,remove_stopwords
 
     article_sentences = []
     article_sentences = list(doc.sents)
-    #print(article_sentences)
+    article_str_sentences = list(map(str,article_sentences))#change list f spans to list of string
 
     # Removing Stop Words & Punctuation 
     processed_sentences = []
     
-    if not lemmatization:
-        if not remove_stopwords:
-            #doesnt remove stopword and doesnt lemmatize
-            for sentence in article_sentences:
-                tokens = tokenize(sentence.text,stopwords=[""],keep_numbers=True,keep_emails=True,keep_urls=True,)
-                cleaned_text = ' '.join(tokens)
-            return article_sentences, processed_sentences
-        #remove stopword but doesnt lemmatize
-        for sentence in article_sentences:
-                tokens = tokenize(sentence.text,stopwords=STOPWORDS['en'],keep_numbers=True,keep_emails=True,keep_urls=True,)
-                processed_sentences = ' '.join(tokens)
-        return article_sentences, processed_sentences
     #remove stopwords and lemmatize
+    if lemmatization:
+        if remove_stopwords:
+            print("lemmatize and remove stopwords")
+            lemmatized_sentences = lemmatize_sentence(article_sentences , STOPWORDS['en'])
+        else:
+            print("lemmatize but doesnt remove stopwords")
+            lemmatized_sentences = lemmatize_sentence(article_sentences, [''])
+        return article_str_sentences, lemmatized_sentences
+    
+    if remove_stopwords:
+        print("doesnt lemmatize but remove stopwords")
+        processed_sentences = stopwords_removal(article_sentences)
+        return article_str_sentences,processed_sentences
+
+    #doesnt remove stopword and doesnt lemmatize
+    print("doesnt lemmatize or remove stopwords")
+    for sentence in article_sentences:
+        tokens = tokenize(sentence.text,stopwords=[''],keep_numbers=True,keep_emails=True,keep_urls=True,)
+        cleaned_text = ' '.join(tokens)
+        processed_sentences.append(cleaned_text)
+    
+    return article_str_sentences, processed_sentences
+
+
+def lemmatize_sentence(article_sentences, stopwords_set):
+    nlp = spacy.load('en_core_web_sm')
     lemmatized_sentences = []
     for sentence in article_sentences:
-            tokens = tokenize(sentence.text,stopwords=STOPWORDS['en'],keep_numbers=True,keep_emails=True,keep_urls=True,)
+            tokens = tokenize(sentence.text,stopwords=stopwords_set,keep_numbers=True,keep_emails=True,keep_urls=True,)
             cleaned_text = ' '.join(tokens)
             spacy_tokens = nlp(cleaned_text)
             lemm_tokens = [token.lemma_ for token in spacy_tokens]
             lemm_sentence = ' '.join(lemm_tokens)
             lemmatized_sentences.append(lemm_sentence)
-    
-    processed_str_sentences = lemmatized_sentences
-    article_str_sentences = list(map(str,article_sentences))#change list f spans to list of string
-    return article_str_sentences, processed_str_sentences
+    return lemmatized_sentences
 
 
-def process_df(df):
+def stopwords_removal(article_sentences):
+    processed_sentences = []
+    for sentence in article_sentences:
+        tokens = tokenize(sentence.text,stopwords=STOPWORDS['en'],keep_numbers=True,keep_emails=True,keep_urls=True,)
+        filtered_sentences = ' '.join(tokens)
+        processed_sentences.append(filtered_sentences)
+    return processed_sentences
+
+
+def process_df(df,lemmatization = False,remove_stopwords = False):
     rows,columns = df.shape
     sentences,processed_sentences =[],[] 
     for row in tqdm(range(rows)):
-        articles_sentence , filtered_article = preprocessing_text_with_spacy(df.iloc[row,0])
+        articles_sentence , filtered_article = preprocessing_text_with_spacy(df.iloc[row,0],lemmatization,remove_stopwords)
         sentences.append(articles_sentence)
         processed_sentences.append(filtered_article)
     
